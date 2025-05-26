64e2fa6be43179af77d4f97948a9b873704a4957
Archishmaan Upadhyaya <archishmaan.upa4@gmail.com>
2025-05-26 20:26:06 +0530

    practiced tailwind and props

--- tailwindprops_frontend/src/App.jsx
+++ tailwindprops_frontend/src/App.jsx
@@ -1,7 +1,21 @@
+import Card from "./components/Card";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const myObjt = {
+    name: "Nike Shoes",
+    btnUrl: "https://www.nike.com/in/w/mens-shoes-nik1zy7ok",
+  };
+  return (
+    <>
+      <div>
+        <h1 className="text-2xl font-bold underline text-black  bg-red-200 p-4 rounded-xl">
+          Hello world!
+        </h1>
+      </div>
+      <Card name={myObjt.name} url={myObjt.btnUrl} />
+    </>
+  );
 }
 
 export default App;
